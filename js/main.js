8c6711ce1168e95311d89c3b3f1b3a8a33bae887
Alexis Berry <alexisberry@Alexiss-MacBook-Pro-2.local>
2018-02-01 22:20:09 -0500

    add array of objects with presidents

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var iceCream = ['chocolate', 'cookies and cream', 'cookie dough'];
+var newLength = iceCream.push('butter pecan');
+
+var presidents = [
+{firstName: 'Donald', lastName: 'Trump', termLength: '1', party: 'Republican', yearsOfPresidency: '1'},
+{firstName: 'Barack', lastName: 'Obama', termLength: '2', party: 'Democrat', yearsOfPresidency: '8'},
+{firstName: 'George', lastName: 'Bush', termLength: '2', party: 'Republican', yearsOfPresidency: '8'},
+{firstName: 'Bill', lastName: 'Clinton', termLength: '2', party: 'Democrat', yearsOfPresidency: '8'},
+{firstName: 'George', lastName: 'Bush', termLength: '1', party: 'Republican', yearsOfPresidency: '4'}
+];
